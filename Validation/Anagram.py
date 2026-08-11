398c36f0ca3586bed00fd53004c55a05aab9fa8f
tg968258-glitch <tg968258@gmail.com>
2026-08-11 18:34:09 +0530

    Delete Validation directory

--- Validation/Anagram.py
+++
@@ -1,6 +0,0 @@
-first=input("Enter a string:")
-second=input("Enter another string:")
-if sorted(first)==sorted(second):
-   print("Valid Anagram")
-else:
-   print("Not an Anagram")
--- Validation/Password_Validation.py
+++
@@ -1,8 +0,0 @@
-from utils.input_validator import (validate_length)
-
-password = input("Enter password: ")
-
-if validate_length(password, 8, 16):
-    print("Password Accepted")
-else:
-    print("Password must be between 8 and 16 characters.")
